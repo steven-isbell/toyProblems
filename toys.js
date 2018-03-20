d8e23f3d9b2311794b33257c36772c1464cba290
Steven Isbell <steveni_92@msn.com>
2018-03-20 01:26:01 -0500

    Update toys.js

--- toys.js
+++ toys.js
@@ -1777,3 +1777,15 @@ String.prototype.characterCount = function (charsToCount) {
   return mapped.map(val => val[Object.keys(val)[0]])
   
 };
+
+// The Math.min function has stopped working, so we have to use our own function. We are off to a good start, but this function doesn't seem to handle everything properly. Add in the proper checks to return NaN for anything that isn't an actual number, except treat null like 0.
+
+function min(a, b){
+  if(a === null)
+    a = 0;
+  if (b === null)
+    b = 0;
+  if(isNaN(a) || isNaN(b))
+    return NaN;
+  return (a<b)?a:b;
+}
