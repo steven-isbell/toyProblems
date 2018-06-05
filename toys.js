589f77dc29607fdf6eaed239cca3a006747607f7
Steven Isbell <steveni_92@msn.com>
2018-06-05 09:32:57 -0500

    Update toys.js

--- toys.js
+++ toys.js
@@ -2015,3 +2015,18 @@ function swap(arr, i, j) {
   arr[i] = arr[j];
   arr[j] = temp;
 }
+
+// Given a string of words, you need to find the highest scoring word.
+// Each letter of a word scores points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc.
+// You need to return the highest scoring word as a string.
+// If two words score the same, return the word that appears earliest in the original string.
+// All letters will be lowercase and all inputs will be valid.
+
+function high(x){
+  const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  const words = x.split(' ');
+  const leaderboard = {};
+  words.forEach(val => leaderboard[val] = val.split('').reduce((acc, cur) => acc + (letters.indexOf(cur) + 1), 0));
+  const max = Math.max(...Object.values(leaderboard));
+  return Object.entries(leaderboard).map((val) => val[1] === max ? val[0] : null).filter(val => val !== null)[0];
+}
