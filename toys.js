d59dd4396f83339e31fcc6dbd33e797e93536670
Steven Isbell <steveni_92@msn.com>
2018-10-24 11:50:13 -0500

    Update toys.js

--- toys.js
+++ toys.js
@@ -2117,3 +2117,9 @@ function maxAndMin(arr1,arr2){
   
   return [largest, smallest];
 }
+
+// Write a function getChar/GetChar/get_char which takes a number and returns the corresponding ASCII char for that value.
+
+function getChar(c){
+  return String.fromCharCode(c);
+}
