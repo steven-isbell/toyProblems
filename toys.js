621b4d824ca83dd45003232b56e8a8f7a6a101bc
Steven Isbell <steveni_92@msn.com>
2019-05-19 23:06:45 -0600

    Update toys.js

--- toys.js
+++ toys.js
@@ -2123,3 +2123,20 @@ function maxAndMin(arr1,arr2){
 function getChar(c){
   return String.fromCharCode(c);
 }
+
+// Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+// For example: ["3:1", "2:2", "0:1", ...]
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+// if x>y - 3 points
+// if x<y - 0 point
+// if x=y - 1 point
+
+function points(games) {
+  return games.reduce((acc, cur) => {
+    const diff = cur.split(':');
+    if (diff[0] > diff[1]) return acc += 3;
+    else if (diff[0] < diff[1]) return acc;
+    else return acc += 1;
+  }, 0)
+}
