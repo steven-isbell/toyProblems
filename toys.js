158545484ec970f418535f7584df9fdb2a42b18a
Steven Isbell <steveni_92@msn.com>
2019-05-30 22:55:15 -0700

    Update toys.js

--- toys.js
+++ toys.js
@@ -2139,3 +2139,20 @@ function points(games) {
     else return acc += 1;
   }, 0)
 }
+
+// Find Prime
+
+function isPrime(num) {
+ if (num <= 1) return false;
+
+ if (num <= 3) return true;
+
+ if (num % 2 == 0 || num % 3 == 0) return false;
+
+ for (let i = 5; i * i <= num; i = i + 6) {
+    if (num % i === 0 || num % (i + 2) === 0)
+    return false;
+  }
+
+ return true;
+}
