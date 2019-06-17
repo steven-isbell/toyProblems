cd7ad4e47ed1d63db52090807c8c4a9d0b97bb3b
Steven Isbell <steveni_92@msn.com>
2019-06-17 00:08:20 -0600

    Update toys.js

--- toys.js
+++ toys.js
@@ -2198,3 +2198,14 @@ function partsSums(ls) {
    }
    return [...container, 0];
 }
+
+// Write a function that accepts a string, and returns true if it is in the form of a phone number. 
+// Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+
+// Only worry about the following format:
+// (123) 456-7890 (don't forget the space after the close parentheses) 
+
+function validPhoneNumber(phoneNumber){
+  const phoneNumberPattern = /^\((\d{3})\)[ ](\d{3})[-](\d{4})$/;
+  return phoneNumberPattern.test(phoneNumber);
+}
