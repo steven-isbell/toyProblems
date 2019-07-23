c209e80323ac678e0a8314c31114dcc2e28c1cdb
Steven Isbell <steveni_92@msn.com>
2019-07-22 19:47:43 -0600

    Update toys.js

--- toys.js
+++ toys.js
@@ -2217,3 +2217,11 @@ function solution(value){
   const append = `0000${value}`.slice(-5)
   return `Value is ${append}`
 }
+
+// Your task is to make a function that can take any non-negative integer as a argument and return it with its
+// digits in descending order.
+// Essentially, rearrange the digits to create the highest possible number.
+
+function descendingOrder(n){
+  return parseInt(('' + n).split('').sort((a, b) => b - a).join(''), 10);
+}
